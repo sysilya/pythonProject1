51d935c8b982dc70719796dd8e429c82fba4aeb0
sysilya <91081701+sysilya@users.noreply.github.com>
2021-09-27 14:58:23 +0300

    port

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 FROM ubuntu:16.04
 RUN apt-get update
 RUN apt-get install nginx -y
-EXPOSE 8080
+EXPOSE 80
 RUN rm -rf /var/www/html/*
 ADD index.html /var/www/html/
 CMD ["nginx", "-g", daemon off;"
